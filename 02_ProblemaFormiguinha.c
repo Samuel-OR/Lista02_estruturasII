8a874bbb57e8a35de1351187ca7b79773baef146
Samuel-OR <samueloliveira0014@gmail.com>
2019-11-05 16:50:53 -0300

    Versão Final

--- 02_ProblemaFormiguinha.c
+++ 02_ProblemaFormiguinha.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define TAM 4
 typedef struct{
     int nro_vertices;
     int eh_ponderado;
@@ -13,6 +12,30 @@ struct pilha{
     struct pilha *prox;
 };
 typedef struct pilha Pilha;
+
+Grafo* cria_Grafo(int nro_vertices){
+    Grafo *gr;
+    gr = (Grafo*) malloc(sizeof(Grafo));
+    if( gr != NULL){
+        gr->nro_vertices = nro_vertices;
+        gr->grau=(int*)calloc(nro_vertices, sizeof(int));
+        gr->arestas = (int**)malloc(nro_vertices*sizeof(int*));
+        for(int i=0; i<nro_vertices; i++){
+            gr->arestas[i] = (int*)calloc(nro_vertices, sizeof(int));
+        }
+    }
+    return gr;
+}
+void insereAresta(Grafo **gr, int origem, int destino){
+    if(*gr != NULL){
+        if(origem >= 0 && origem <= (*gr)->nro_vertices){
+            if(destino > 0 && destino <= (*gr)->nro_vertices){
+                (*gr)->arestas[origem][(*gr)->grau[origem]] = destino;
+            }
+            (*gr)->grau[origem]++;
+        }
+    }
+}
 
 int **gerarEstados(){
     int **estados = (int**)malloc(sizeof(int*)*31);
@@ -42,6 +65,7 @@ int **gerarEstados(){
                 estados[cont][0] = x;
                 estados[cont][1] = y;
                 estados[cont][2] = z;
+                estados[cont][3] = 2;
                 cont++;
             }
         }
@@ -51,66 +75,32 @@ int **gerarEstados(){
         for(int y=-1; y < 2; y+=2){
             estados[cont][0] = x;
             estados[cont][1] = y;
+            estados[cont][2] = 2;
+            estados[cont][3] = 2;
             cont++;
         }
     }
 
     for(int x=-1; x < 2; x+=2){
         estados[cont][0] = x;
+        estados[cont][1] = 2;
+        estados[cont][2] = 2;
+        estados[cont][3] = 2;
         cont++;
     }
     estados[cont][0] = 0;
+    estados[cont][1] = 2;
+    estados[cont][2] = 2;
+    estados[cont][3] = 2;
     return estados;
 }
-
-int mostrarEstados(int **estados,int indiceOUTodos){
-    if( indiceOUTodos == 31){
-        for(int x=0; x<31; x++){
-            printf("%d | %d %d %d %d\n",x, estados[x][0], estados[x][1], estados[x][2], estados[x][3] );
-        }
-    }else{
-        printf("%d | %d %d %d %d\n",indiceOUTodos
-            ,estados[indiceOUTodos][0], estados[indiceOUTodos][1]
-            ,estados[indiceOUTodos][2], estados[indiceOUTodos][3]
-            );
-    }
-}
-
-Grafo* cria_Grafo(int nro_vertices){
-    Grafo *gr;
-    gr = (Grafo*) malloc(sizeof(Grafo));
-    if( gr != NULL){
-        gr->nro_vertices = nro_vertices;
-        gr->grau=(int*)calloc(nro_vertices, sizeof(int));
-        gr->arestas = (int**)malloc(nro_vertices*sizeof(int*));
-        for(int i=0; i<nro_vertices; i++){
-            gr->arestas[i] = (int*)calloc(nro_vertices, sizeof(int));
-        }
-    }
-    return gr;
-}
-
-void insereAresta(Grafo **gr, int origem, int destino){
-    if(*gr != NULL){
-        if(origem >= 0 && origem <= (*gr)->nro_vertices){
-            if(destino > 0 && destino <= (*gr)->nro_vertices){
-                (*gr)->arestas[origem][(*gr)->grau[origem]] = destino;
-            }
-            (*gr)->grau[origem]++;
-        }
-    }
-}
-
-
-
-int tamaEst(int *estado){
+int Length(int *estado){
     int tam=0;
     for(tam=0;estado[tam]!=0;tam++);
     return tam;
 }
-
 int igual(int *est,int *est2){
-    int t=tamaEst(est2);
+    int t=Length(est2);
     int retorno=1;
     for(int i=0;i<t && retorno==1;i++){
         if(est[i]!=est2[i]) retorno=0;
@@ -118,27 +108,21 @@ int igual(int *est,int *est2){
     return retorno;
 }
 
-
-
-
-//#############
-int temColisao(int *estado){
+int FormigaBatendo(int *estado){
     int resul=0;
-    int tam=tamaEst(estado);
+    int tam=Length(estado);
     for(int i=0;i<tam-1 && resul==0;i++)
         if(estado[i]==1 && estado[i+1]==-1) resul=1;
     return resul;
 }
-
-int temSaida(int *estado){
+int FormigaDeSaida(int *estado){
     int retorno=0;
-    int tam=tamaEst(estado);
+    int tam=Length(estado);
     if(estado[0]==-1 || estado[tam-1]==1) retorno=1;
     return retorno;
 }
-
-int *tira(int *estado){
-    int ini=0,end=tamaEst(estado),novoTam=end;
+int *TiraFormiga(int *estado){
+    int ini=0,end=Length(estado),novoTam=end;
     if(estado[ini]==-1){
         novoTam-=1;
         ini=1;
@@ -154,13 +138,11 @@ int *tira(int *estado){
     }
     return NovoEstado;
 }
-
-int *tiraC(int *estado){
-    int tam=tamaEst(estado);
+int *TiraColisaoFormiga(int *estado){
+    int tam=Length(estado);
     int *prox=(int*)malloc(sizeof(int*)*tam);
-    for(int i=0;i<tam;i++){
+    for(int i=0;i<tam;i++)
         prox[i]=estado[i];
-    }
     for(int i=0;i<tam-1;i++){
         if(prox[i]==1 && prox[i+1]==-1){
             prox[i]*=-1;
@@ -170,23 +152,20 @@ int *tiraC(int *estado){
     }
     return prox;
 }
-
 int *EstadoAdj(int *estAtual){
     int *prox;
-    if(temColisao(estAtual)){
-        if(temSaida(estAtual)){
-            prox=tira(estAtual);
+    if(FormigaBatendo(estAtual)){
+        if(FormigaDeSaida(estAtual)){
+            prox=TiraFormiga(estAtual);
             estAtual=prox;
         }
-        prox=tiraC(estAtual);
-
-    }else if(temSaida(estAtual)){
-        prox=tira(estAtual);
+        prox=TiraColisaoFormiga(estAtual);
+    }else if(FormigaDeSaida(estAtual)){
+        prox=TiraFormiga(estAtual);
     }
     return prox;
 }
 
-//#############
 int equivalenteIndice(int **estado,int *vet){
     int resul = 0;
     for(int i=0; i< 31; i++){
@@ -199,26 +178,22 @@ int equivalenteIndice(int **estado,int *vet){
     }
     return resul;
 }
-
 int equivalenteEstado(int* est1, int* est2){
-    int t=tamaEst(est1);
+    int t=Length(est1);
     int retorno=1;
     for(int i=0;i<t && retorno==1;i++){
         if(est1[i]!=est2[i]) retorno=0;
     }
     return retorno;
 }
-
 int equivalenteTamanho(int* est1, int* est2){
     int resul = 0;
-    int t1=tamaEst(est1);
-    int t2=tamaEst(est2);
+    int t1=Length(est1);
+    int t2=Length(est2);
     if(t1==t1)
         resul = 1;
     return resul;
 }
-
-
 int EstadoDestino(int **estados,int *estado){
     int resul = -1;
     for(int i=0;i<31 && resul==-1;i++){
@@ -242,28 +217,47 @@ void construirGrafo(Grafo *gr,int **estados, int qtd){
 
     }
 }
-
-//#########################
-void push(int v, Pilha **p){
+void appendPilha(int v, Pilha **p){
     Pilha *cel=(Pilha*)malloc(sizeof(Pilha));
     cel->n=v;
     cel->prox=*p;
     *p=cel;
 }
-
 void buscarCaminho(Grafo *gr, int inicio, Pilha **pilha, int *cont ){
-    push(inicio,pilha);
+    appendPilha(inicio,pilha);
     for(int i=0; i<gr->grau[inicio]; i++){
         *cont+=1;
         buscarCaminho(gr, gr->arestas[inicio][i], pilha, cont);
     }
 }
 
-void exibePilha(Pilha *P,int **est){
+void mostrarEstados(int **estados,int indiceOUTodos){
+    if( indiceOUTodos == 31){
+        for(int x=0; x<31; x++){
+            if(estados[x][3] != 2)
+                printf("%d | %d %d %d %d\n",x, estados[x][0], estados[x][1], estados[x][2], estados[x][3] );
+            else if(estados[x][2] != 2)
+                printf("%d | %d %d %d\n",x, estados[x][0], estados[x][1], estados[x][2]);
+            else if(estados[x][1] != 2)
+                printf("%d | %d %d\n",x, estados[x][0], estados[x][1]);
+            else if(estados[x][0] != 2)
+                printf("%d | %d\n",x, estados[x][0]);
+        }
+    }else{
+        if(estados[indiceOUTodos][3] != 2)
+            printf("%d | %d %d %d %d\n",indiceOUTodos, estados[indiceOUTodos][0], estados[indiceOUTodos][1], estados[indiceOUTodos][2], estados[indiceOUTodos][3] );
+        else if(estados[indiceOUTodos][2] != 2)
+            printf("%d | %d %d %d\n",indiceOUTodos, estados[indiceOUTodos][0], estados[indiceOUTodos][1], estados[indiceOUTodos][2]);
+        else if(estados[indiceOUTodos][1] != 2)
+            printf("%d | %d %d\n",indiceOUTodos, estados[indiceOUTodos][0], estados[indiceOUTodos][1]);
+        else if(estados[indiceOUTodos][0] != 2)
+            printf("%d | %d\n",indiceOUTodos, estados[indiceOUTodos][0]);
+    }
+}
+void mostrarPilha(Pilha *P,int **est){
     Pilha *aux = P;
     if(aux!=NULL){
-        exibePilha(P->prox,est);
-        printf("%d:",aux->n);
+        mostrarPilha(P->prox,est);
         mostrarEstados(est, aux->n);
     }
 }
@@ -271,38 +265,21 @@ void exibePilha(Pilha *P,int **est){
 int main(){
     
     Pilha *pilha=NULL;
-    int **estados, vet[TAM], cont;
+    int **estados, vet[4], qtd;
 
     Grafo *grafo = cria_Grafo(31);
-    estados = gerarEstados(TAM);
+    estados = gerarEstados(4);
     
     //mostrarEstados(estados, 31);
     construirGrafo(grafo,estados, 30);
-    
 
-    /*for(int i=0;i<31;i++){
-        printf("Estado ");
-        printf(" [");
-        mostrarEstados(estados, i);
-        printf("] ");
-        for(int j=0;j<grafo->grau[i];j++){
-            printf("Vai para o Estado ");
-            mostrarEstados(estados, grafo->arestas[i][j]);
-        }
-        printf("\n");
-    }*/
-
-
-    printf("Insira posição inicial (ex: [1 -1 1 -1].\n");
-    printf("->");
+    printf("Insira posição inicial (ex: [1 -1 1 -1] => ");
     scanf("%d %d %d %d", &vet[0],&vet[1],&vet[2],&vet[3]);
     int inicio = equivalenteIndice(estados,vet);
 
-   
-
-    buscarCaminho(grafo, inicio, &pilha, &cont);
-    exibePilha(pilha, estados);
-    
+    buscarCaminho(grafo, inicio, &pilha, &qtd);
+    mostrarPilha(pilha, estados);
+    printf("Qtd passos: %d\n",qtd );
 
     return 0;
 }
